c865e3c9e57aee326c010466f4d08a82e21191f8
MASSamit <107047162+MASSamit@users.noreply.github.com>
2024-01-05 19:56:47 +0600

    Update Blast_The_Green.cpp

--- Blast_The_Green.cpp
+++ Blast_The_Green.cpp
@@ -4,7 +4,15 @@
 #include<conio.h>
 #include<iostream>
 using namespace std;
+
+const int SCREEN_WIDTH=40;
+const int SCREEN_HEIGHT=25;
+const int NUM_BALLOONS=3;
+const int PIN_SPEED=5;
+const int BALLOON_SPEED=1;
+
 int main(){
+
   
 return 0;
 }
