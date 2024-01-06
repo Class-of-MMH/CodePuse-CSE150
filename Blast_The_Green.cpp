38a3fae8f29b5c2d3c2cf7ec7f671b238c0d357f
MASSamit <107047162+MASSamit@users.noreply.github.com>
2024-01-06 23:30:16 +0600

    Update Blast_The_Green.cpp

--- Blast_The_Green.cpp
+++ Blast_The_Green.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 const int SCREEN_WIDTH=43;
 const int SCREEN_HEIGHT=27;
-const int NUM_BALLOONS=3;
+const int NUM_BALLOONS=4;
 const int PIN_SPEED=5;
 const int BALLOON_SPEED=1;
 
@@ -116,7 +116,7 @@ system("pause");
 srand(static_cast<unsigned>(time(0)));
 char*buffer=new char[SCREEN_WIDTH*SCREEN_HEIGHT];
 memset(buffer, ' ', SCREEN_WIDTH*SCREEN_HEIGHT);
-Balloon balloons[NUM_BALLOONS]={'g', 'r', 'g'};
+Balloon balloons[NUM_BALLOONS]={'g', 'r', 'g','r'};
 Pin pin;
   
 int balloonTimer=0;
